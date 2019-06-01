0d48cc6c87fe872d468c03a615165194db245f01
Милана Бетирова <mila.zver@mail.ru>
2019-06-01 17:06:04 +0400

    sad

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-heroku = Heroku(app)
 migrate = Migrate(app, db)
 
 from app.modules import routes
